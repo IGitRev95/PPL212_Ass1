51fdce66df84f43d46156e129eed70957cfaad49
Ido <idorev@post.bgu.ac.il>
2021-03-13 21:18:33 +0200

    Co-authored-by: naveben <naveben@users.noreply.github.com>

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -3,10 +3,28 @@ import * as R from "ramda";
 const stringToArray = R.split("");
 
 /* Question 1 */
-export const countVowels = undefined;
+export const countVowels = (txt: string): number =>{
+    return stringToArray(txt).filter(ch=>
+        ch==='a' ? true :
+        ch==='A' ? true :
+        ch==='e' ? true :
+        ch==='E' ? true :
+        ch==='i' ? true :
+        ch==='I' ? true :
+        ch==='o' ? true :
+        ch==='O' ? true :
+        ch==='u' ? true :
+        ch==='U' ? true : false
+    ).length; 
+};
 
 /* Question 2 */
-export const runLengthEncoding = undefined;
+export const runLengthEncoding = (txt: string): string => {
+
+    let A: Array<string>= stringToArray(txt);
+    console.log(Json.stringify(A));
+    console.log(["a","a","b","b"].reduce((acc,curr)=>acc.concat(curr),[]));
+}
 
 /* Question 3 */
 export const isPaired = undefined;
