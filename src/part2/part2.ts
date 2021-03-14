ba1bd32e254de94756a61fd1dd1741a7b0f37000
Ido <idorev@post.bgu.ac.il>
2021-03-14 23:06:29 +0200

    not working need debug q3

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -3,31 +3,63 @@ import * as R from "ramda";
 const stringToArray = R.split("");
 
 /* Question 1 */
-export const countVowels = (txt: string): number =>{
-    return stringToArray(txt).filter(ch=>
-        ch==='a' ? true :
-        ch==='A' ? true :
-        ch==='e' ? true :
-        ch==='E' ? true :
-        ch==='i' ? true :
-        ch==='I' ? true :
-        ch==='o' ? true :
-        ch==='O' ? true :
-        ch==='u' ? true :
-        ch==='U' ? true : false
-    ).length; 
-};
+export const countVowels = (txt: string): number =>
+    stringToArray(txt).filter(ch=>R.intersection(['a','A','e','E','i','I','o','O','u','U'],[ch]).length===1 ? true:false).length; 
 
-/* Question 2 */
+/*
+/* Question 2 *
 export const runLengthEncoding = (txt: string): string => {
 
     let A: Array<string>= stringToArray(txt);
     console.log(Json.stringify(A));
     console.log(["a","a","b","b"].reduce((acc,curr)=>acc.concat(curr),[]));
 }
-
+*/
 /* Question 3 */
-export const isPaired = (txt: string): boolean =>{
-    return 
-
-};
+export const isPaired = R.pipe(
+    ( txt: string ) => stringToArray( txt ),
+    ( txtar: string[] ) => txtar.filter( ch => R.intersection(['(',')','{','}','[',']'],[ch])),
+    ( bracketsarr: string[] ) => bracketsarr.reduce( ( acc , cur ) => {
+        const instF=(arr: any[])=>R.insert(0,"false",arr);
+        const removeFirst=(arr: any[])=>R.remove(0,1,arr);
+        switch(cur){
+            case ')':
+                return    acc[0]==='(' ? removeFirst(acc) : instF(acc);
+            case '}':
+                return    acc[0]==='{' ? removeFirst(acc) : instF(acc);
+            case ']':
+                return    acc[0]==='[' ? removeFirst(acc) : instF(acc);
+            case '(':
+            case '{':
+            case '[':
+                return    R.insert(0,cur,acc);
+            default: return acc;
+        }
+    },[""]),
+    ( resultarr: string[] ) => resultarr.filter( ch => R.intersection(['(','{','[','false'],[ch])).length===0 ? true : false
+);
+/*
+const foo=R.pipe(
+    ( txt: string ) => stringToArray( txt ),
+    ( txtar: string[] ) => txtar.filter( ch => R.intersection(['(',')','{','}','[',']'],[ch])),
+    ( bracketsarr: string[] ) => bracketsarr.reduce( ( acc , cur ) => {
+        const instF=(arr: any[])=>R.insert(0,"false",arr);
+        const removeFirst=(arr: any[])=>R.remove(0,1,arr);
+        switch(cur){
+            case ')':
+                return    acc[0]==='(' ? removeFirst(acc) : instF(acc);
+            case '}':
+                return    acc[0]==='{' ? removeFirst(acc) : instF(acc);
+            case ']':
+                return    acc[0]==='[' ? removeFirst(acc) : instF(acc);
+            case '(':
+            case '{':
+            case '[':
+                return    R.insert(0,cur,acc);
+            default: return acc;
+        }
+    },[""]),
+    ( resultarr: string[] ) => resultarr.filter( ch => R.intersection(['(','{','[','false'],[ch])).length===0 ? true : false
+);
+*/
+console.log(isPaired("This is ([some]) {text}"));
