f3ddc22a0f5413935eba871cd11e67440cd14fd9
Ido <idorev@post.bgu.ac.il>
2021-03-18 21:12:38 +0200

    state done, queue 1 fail

--- src/part3/queue.ts
+++ src/part3/queue.ts
@@ -1,9 +1,13 @@
 import { State, bind } from "./state";
-
+import * as R from 'ramda';
 export type Queue = number[];
 
-export const enqueue = undefined;
+export const enqueue = (x:number): State<Queue,number> => {
+    return (oldQueue: Queue) => [R.concat(oldQueue,[x]),x];
+};
 
-export const dequeue = undefined;
+export const dequeue: State<Queue,number> = 
+(oldQueue: Queue) => [R.tail(oldQueue),oldQueue[0]];
 
-export const queueManip = undefined;
+export const queueManip : State<Queue,number> =
+bind(dequeue, (x:number)=>bind(enqueue(x*2),z=>bind(enqueue(x/3),s=>dequeue)));
--- src/part3/state.ts
+++ src/part3/state.ts
@@ -2,7 +2,7 @@ export type State<S, A> = (initialState: S) => [S, A];
 
 export const bind = <S, A, B>(state: State<S, A>, f: (x: A) => State<S, B>): State<S, B> =>{
     return (initialState: S)=> {
-        const curState: [S,A]=state(initialState);
+        const curState: [S,A] = state(initialState);
         return f(curState[1])(curState[0]);
     }
 }
