6a352c1470a23555c689583a6ccc82ad0c93c141
Ido <idorev@post.bgu.ac.il>
2021-03-18 21:27:19 +0200

    finish 1

--- src/part3/stack.ts
+++ src/part3/stack.ts
@@ -1,9 +1,13 @@
 import { State, bind } from "./state";
-
+import * as R from 'ramda';
 export type Stack = number[];
 
-export const push = undefined;
+export const push =  (x:number): State<Stack,undefined> => {
+    return (oldStack: Stack) => [R.concat([x],oldStack),undefined];
+};
 
-export const pop = undefined;
+export const pop: State<Stack,number> = 
+(oldStack: Stack) => [R.tail(oldStack),oldStack[0]];
 
-export const stackManip = undefined;
+export const stackManip : State<Stack,undefined> =
+bind(pop, (x:number)=>bind(push(x**2),z=>bind(pop,s=>push(x+s))));
